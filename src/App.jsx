76e1235d7da76c5ca49db023b6b49843e7023a87
willk <wkencel1@gmail.com>
2023-09-10 12:57:20 -0700

    Integrated particles package and configured
    added the particles component and configured the styling in the particles.json file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
-import { useState } from 'react'
+import { useState, useCallback } from "react";
 import './styles/App.css'
 import '@chatscope/chat-ui-kit-styles/dist/default/styles.min.css';
 import { MainContainer, ChatContainer, MessageList, Message, MessageInput, TypingIndicator } from '@chatscope/chat-ui-kit-react';
 import CsvParser from './components/CsvParser'
-
+import Particles from "react-particles";
+import { loadFull } from "tsparticles";
+import particlesOptions from "../particles.json";
 import Papa from "papaparse";
 
 function App() {
+  const particlesInit = useCallback((main) => {
+    loadFull(main);
+  }, []);
   const [messages, setMessages] = useState([
     {
       message: "Hello, I'm ChatGPT! Ask me anything!",
@@ -21,36 +26,36 @@ function App() {
     role: "system",
     content: `Explain things like you're a data analyst with 10 years experience and we are looking at this data ${JSON.stringify(
       csvData
-    )}`,
-  };
-
-  // Handle the error if no API Key
-  if (!import.meta.env.VITE_OPENAI_API_KEY) {
-    console.error("ERROR: API_KEY environment variable is not set.");
-  }
-
-  const handleSend = async (message) => {
-    const newMessage = {
-      message,
+      )}`,
+    };
+    
+    // Handle the error if no API Key
+    if (!import.meta.env.VITE_OPENAI_API_KEY) {
+      console.error("ERROR: API_KEY environment variable is not set.");
+    }
+    
+    const handleSend = async (message) => {
+      const newMessage = {
+        message,
       direction: 'outgoing',
       sender: "user"
     };
-
+    
     const newMessages = [...messages, newMessage];
     
     setMessages(newMessages);
-
+    
     // Initial system message to determine ChatGPT functionality
     // How it responds, how it talks, etc.
     setIsTyping(true);
     await processMessageToChatGPT(newMessages);
   };
-
+  
   async function processMessageToChatGPT(chatMessages) { // messages is an array of messages
     // Format messages for chatGPT API
     // API is expecting objects in format of { role: "user" or "assistant", "content": "message here"}
     // So we need to reformat
-
+    
     let apiMessages = chatMessages.map((messageObject) => {
       let role = "";
       if (messageObject.sender === "ChatGPT") {
@@ -60,8 +65,8 @@ function App() {
       }
       return { role: role, content: messageObject.message}
     });
-
-
+    
+    
     // Get the request body set up with the model we plan to use
     // and the messages which we formatted above. We add a system message in the front to'
     // determine how we want chatGPT to act. 
@@ -72,7 +77,7 @@ function App() {
         ...apiMessages // The messages from our chat with ChatGPT
       ]
     }
-
+    
     await fetch("https://api.openai.com/v1/chat/completions", {
       method: "POST",
       headers: {
@@ -81,56 +86,70 @@ function App() {
       },
       body: JSON.stringify(apiRequestBody),
     })
-      .then((data) => {
-        console.log(`hit console.log #1, data: ${data}`);
-        return data.json();
-      })
-      .then((data) => {
-        console.log(`hit console.log #2, data: ${data}`);
-        setMessages([
-          ...chatMessages,
-          {
-            message: data.choices[0].message.content,
-            sender: "ChatGPT",
-          },
-        ]);
-        setIsTyping(false);
-      });
-  }
-
-    const handleFileUpload = (e) => {
-      const file = e.target.files[0];
-      Papa.parse(file, {
-        header: true,
-        complete: (results) => {
-          setCsvData(results.data);
+    .then((data) => {
+      console.log(`hit console.log #1, data: ${data}`);
+      return data.json();
+    })
+    .then((data) => {
+      console.log(`hit console.log #2, data: ${data}`);
+      setMessages([
+        ...chatMessages,
+        {
+          message: data.choices[0].message.content,
+          sender: "ChatGPT",
         },
-      });
-    };
-
+      ]);
+      setIsTyping(false);
+    });
+  }
+  
+  const handleFileUpload = (e) => {
+    const file = e.target.files[0];
+    Papa.parse(file, {
+      header: true,
+      complete: (results) => {
+        setCsvData(results.data);
+      },
+    });
+  };
+  
   return (
     <div className="App">
+      <Particles options={particlesOptions} init={particlesInit} />;
       <div style={{ position: "relative", height: "800px", width: "700px" }}>
-        <input type="file" accept=".csv" onChange={handleFileUpload} />
-        <MainContainer>
-          <ChatContainer>
-            <MessageList
-              scrollBehavior="smooth"
-              typingIndicator={
-                isTyping ? (
-                  <TypingIndicator content="ChatGPT is typing" />
+        <input
+          type="file"
+          accept=".csv"
+          onChange={handleFileUpload}
+          style={{ position: "relative", zIndex: 2 }}
+        />
+        <div style={{ backgroundColor: "black", position: "relative", zIndex: 2 }} >
+          <MainContainer>
+            <ChatContainer>
+              <MessageList
+                scrollBehavior="smooth"
+                typingIndicator={
+                  isTyping ? (
+                    <TypingIndicator content="ChatGPT is typing" />
                   ) : null
-              }
-            >
-            {messages.map((message, i) => {
-              console.log(`chatgpt message ${message}`);
-              return <Message key={i} model={message} />;
-            })}
-            </MessageList>
-            <MessageInput placeholder="Type message here" onSend={handleSend} />
-          </ChatContainer>
-        </MainContainer>
-        <CsvParser csv={csvData} />
+                }
+              >
+                {messages.map((message, i) => {
+                  console.log(`chatgpt message ${message}`);
+                  return <Message key={i} model={message} />;
+                })}
+              </MessageList>
+              <MessageInput
+                placeholder="Type message here"
+                onSend={handleSend}
+              />
+            </ChatContainer>
+          </MainContainer>
+          <CsvParser
+            csv={csvData}
+            style={{ position: "relative", zIndex: 2 }}
+          />
+        </div>
       </div>
     </div>
   );
